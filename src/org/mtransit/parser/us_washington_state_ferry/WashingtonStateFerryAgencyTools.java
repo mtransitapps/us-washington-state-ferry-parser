b8570a8e1696d6b35445747250b68006824df5f5
Mathieu Méa <mathieu.mea@gmail.com>
2016-01-22 17:59:58 -0500

    Compatibility with January 22 update.

--- src/org/mtransit/parser/us_washington_state_ferry/WashingtonStateFerryAgencyTools.java
+++ src/org/mtransit/parser/us_washington_state_ferry/WashingtonStateFerryAgencyTools.java
@@ -79,27 +79,28 @@ public class WashingtonStateFerryAgencyTools extends DefaultAgencyTools {
 		return MAgency.ROUTE_TYPE_FERRY;
 	}
 
-	private static final String SPACE = " ";
+	private static final String SEP = " ";
 
 	private static final String ANACORTES_RSN = "ANC";
-	private static final String BAINBRIDGE_ISLAND_RSN = "BNB";
+	private static final String BAINBRIDGE_ISLAND_RSN = "BBR";
 	private static final String BREMERTON_RSN = "BRM";
-	private static final String CLINTON_RSN = "CLN";
+	private static final String CLINTON_RSN = "CLT";
 	private static final String COUPEVILLE_RSN = "CPV";
 	private static final String EDMONDS_RSN = "EDM";
-	private static final String FAUNTLEROY_RSN = "FNT";
-	private static final String FRIDAY_HARBOR_RSN = "FRD";
-	private static final String KINGSTON_RSN = "KNG";
-	private static final String LOPEZ_ISLAND_RSN = "LPZ";
-	private static final String MULKITEO_RSN = "MLK";
-	private static final String ORCAS_ISLAND_RSN = "ORC";
+	private static final String FAUNTLEROY_RSN = "FLR";
+	private static final String FRIDAY_HARBOR_RSN = "FDH";
+	private static final String KINGSTON_RSN = "KGS";
+	private static final String LOPEZ_ISLAND_RSN = "LPI";
+	private static final String MULKITEO_RSN = "MKT";
+	private static final String ORCAS_ISLAND_RSN = "OCI";
 	private static final String POINT_DEFIANCE_RSN = "PDF";
-	private static final String PORT_TOWNSEND_RSN = "PTW";
+	private static final String PORT_TOWNSEND_RSN = "PTS";
 	private static final String SEATTLE_RSN = "STL";
-	private static final String SHAW_ISLAND_RSN = "SWI";
-	private static final String SOUTHWORTH_RSN = "STH";
-	private static final String TAHLEQUAH_RSN = "THL";
-	private static final String VASHON_ISLAND_RSN = "VSH";
+	private static final String SHAW_ISLAND_RSN = "SHI";
+	private static final String SIDNEY_BC_RSN = "SDN";
+	private static final String SOUTHWORTH_RSN = "SWR";
+	private static final String TAHLEQUAH_RSN = "TLQ";
+	private static final String VASHON_ISLAND_RSN = "VSI";
 
 	@Override
 	public String getRouteShortName(GRoute gRoute) {
@@ -107,44 +108,48 @@ public class WashingtonStateFerryAgencyTools extends DefaultAgencyTools {
 			int rsn = Integer.parseInt(gRoute.getRouteId());
 			switch (rsn) {
 			// @formatter:off
-			case 37: return BAINBRIDGE_ISLAND_RSN + SPACE + SEATTLE_RSN; // Bainbridge Island - Seattle
-			case 47: return BREMERTON_RSN + SPACE + SEATTLE_RSN; // Bremerton - Seattle
-			case 73: return SEATTLE_RSN + SPACE + BAINBRIDGE_ISLAND_RSN; // Seattle - Bainbridge Island
-			case 74: return SEATTLE_RSN + SPACE + BREMERTON_RSN; // Seattle - Bremerton
-			case 101: return FRIDAY_HARBOR_RSN + SPACE + ANACORTES_RSN; // Friday Harbor - Anacortes
-			case 110: return ANACORTES_RSN + SPACE + FRIDAY_HARBOR_RSN; // Anacortes - Friday Harbor
-			case 113: return ANACORTES_RSN + SPACE + LOPEZ_ISLAND_RSN; // Anacortes - Lopez Island
-			case 115: return ANACORTES_RSN + SPACE + ORCAS_ISLAND_RSN; // Anacortes - Orcas Island
-			case 118: return ANACORTES_RSN + SPACE + SHAW_ISLAND_RSN; // Anacortes - Shaw Island
-			case 128: return KINGSTON_RSN + SPACE + EDMONDS_RSN; // Kingston - Edmonds
-			case 131: return LOPEZ_ISLAND_RSN + SPACE + ANACORTES_RSN; // Lopez Island - Anacortes
-			case 145: return MULKITEO_RSN + SPACE + CLINTON_RSN; // Mukilteo - Clinton
-			case 151: return ORCAS_ISLAND_RSN + SPACE + ANACORTES_RSN; // Orcas Island - Anacortes
-			case 181: return SHAW_ISLAND_RSN + SPACE + ANACORTES_RSN; // Shaw Island - Anacortes
-			case 209: return SOUTHWORTH_RSN + SPACE + FAUNTLEROY_RSN; // Southworth - Fauntleroy
-			case 229: return VASHON_ISLAND_RSN + SPACE + FAUNTLEROY_RSN; // Vashon Island - Fauntleroy
-			case 514: return CLINTON_RSN + SPACE + MULKITEO_RSN; // Clinton - Mukilteo
-			case 812: return EDMONDS_RSN + SPACE + KINGSTON_RSN; // Edmonds - Kingston
-			case 920: return FAUNTLEROY_RSN + SPACE + SOUTHWORTH_RSN; // Fauntleroy - Southworth
-			case 922: return FAUNTLEROY_RSN + SPACE + VASHON_ISLAND_RSN; // Fauntleroy - Vashon Island
-			case 1013: return FRIDAY_HARBOR_RSN + SPACE + LOPEZ_ISLAND_RSN; // Friday Harbor - Lopez Island
-			case 1015: return FRIDAY_HARBOR_RSN + SPACE + ORCAS_ISLAND_RSN; // Friday Harbor - Orcas Island
-			case 1018: return FRIDAY_HARBOR_RSN + SPACE + SHAW_ISLAND_RSN; // Friday Harbor - Shaw Island
-			case 1117: return COUPEVILLE_RSN + SPACE + PORT_TOWNSEND_RSN; // Coupeville - Port Townsend
-			case 1310: return LOPEZ_ISLAND_RSN + SPACE + FRIDAY_HARBOR_RSN; // Lopez Island - Friday Harbor
-			case 1315: return LOPEZ_ISLAND_RSN + SPACE + ORCAS_ISLAND_RSN; // Lopez Island - Orcas Island
-			case 1318: return LOPEZ_ISLAND_RSN + SPACE + SHAW_ISLAND_RSN; // Lopez Island - Shaw Island
-			case 1510: return ORCAS_ISLAND_RSN + SPACE + FRIDAY_HARBOR_RSN; // Orcas Island - Friday Harbor
-			case 1513: return ORCAS_ISLAND_RSN + SPACE + LOPEZ_ISLAND_RSN; // Orcas Island - Lopez Island
-			case 1518: return ORCAS_ISLAND_RSN + SPACE + SHAW_ISLAND_RSN; // Orcas Island - Shaw Island
-			case 1621: return POINT_DEFIANCE_RSN + SPACE + TAHLEQUAH_RSN; // Point Defiance - Tahlequah
-			case 1711: return PORT_TOWNSEND_RSN + SPACE + COUPEVILLE_RSN; // Port Townsend - Coupeville
-			case 1810: return SHAW_ISLAND_RSN + SPACE + FRIDAY_HARBOR_RSN; // Shaw Island - Friday Harbor
-			case 1813: return SHAW_ISLAND_RSN + SPACE + LOPEZ_ISLAND_RSN; // Shaw Island - Lopez Island
-			case 1815: return SHAW_ISLAND_RSN + SPACE + ORCAS_ISLAND_RSN; // Shaw Island - Orcas Island
-			case 2022: return SOUTHWORTH_RSN + SPACE + VASHON_ISLAND_RSN; // Southworth - Vashon Island
-			case 2116: return TAHLEQUAH_RSN + SPACE + POINT_DEFIANCE_RSN; // Tahlequah - Point Defiance
-			case 2220: return VASHON_ISLAND_RSN + SPACE + SOUTHWORTH_RSN; // Vashon Island - Southworth
+			case 37: return BAINBRIDGE_ISLAND_RSN + SEP + SEATTLE_RSN; // Bainbridge Island - Seattle
+			case 47: return BREMERTON_RSN + SEP + SEATTLE_RSN; // Bremerton - Seattle
+			case 73: return SEATTLE_RSN + SEP + BAINBRIDGE_ISLAND_RSN; // Seattle - Bainbridge Island
+			case 74: return SEATTLE_RSN + SEP + BREMERTON_RSN; // Seattle - Bremerton
+			case 101: return FRIDAY_HARBOR_RSN + SEP + ANACORTES_RSN; // Friday Harbor - Anacortes
+			case 110: return ANACORTES_RSN + SEP + FRIDAY_HARBOR_RSN; // Anacortes - Friday Harbor
+			case 113: return ANACORTES_RSN + SEP + LOPEZ_ISLAND_RSN; // Anacortes - Lopez Island
+			case 115: return ANACORTES_RSN + SEP + ORCAS_ISLAND_RSN; // Anacortes - Orcas Island
+			case 118: return ANACORTES_RSN + SEP + SHAW_ISLAND_RSN; // Anacortes - Shaw Island
+			case 119: return ANACORTES_RSN + SEP + SIDNEY_BC_RSN ; // Anacortes - Sidney B.C.
+			case 128: return KINGSTON_RSN + SEP + EDMONDS_RSN; // Kingston - Edmonds
+			case 131: return LOPEZ_ISLAND_RSN + SEP + ANACORTES_RSN; // Lopez Island - Anacortes
+			case 145: return MULKITEO_RSN + SEP + CLINTON_RSN; // Mukilteo - Clinton
+			case 151: return ORCAS_ISLAND_RSN + SEP + ANACORTES_RSN; // Orcas Island - Anacortes
+			case 181: return SHAW_ISLAND_RSN + SEP + ANACORTES_RSN; // Shaw Island - Anacortes
+			case 191: return SIDNEY_BC_RSN + SEP + ANACORTES_RSN; // Sidney B.C. - Anacortes
+			case 209: return SOUTHWORTH_RSN + SEP + FAUNTLEROY_RSN; // Southworth - Fauntleroy
+			case 229: return VASHON_ISLAND_RSN + SEP + FAUNTLEROY_RSN; // Vashon Island - Fauntleroy
+			case 514: return CLINTON_RSN + SEP + MULKITEO_RSN; // Clinton - Mukilteo
+			case 812: return EDMONDS_RSN + SEP + KINGSTON_RSN; // Edmonds - Kingston
+			case 920: return FAUNTLEROY_RSN + SEP + SOUTHWORTH_RSN; // Fauntleroy - Southworth
+			case 922: return FAUNTLEROY_RSN + SEP + VASHON_ISLAND_RSN; // Fauntleroy - Vashon Island
+			case 1013: return FRIDAY_HARBOR_RSN + SEP + LOPEZ_ISLAND_RSN; // Friday Harbor - Lopez Island
+			case 1015: return FRIDAY_HARBOR_RSN + SEP + ORCAS_ISLAND_RSN; // Friday Harbor - Orcas Island
+			case 1018: return FRIDAY_HARBOR_RSN + SEP + SHAW_ISLAND_RSN; // Friday Harbor - Shaw Island
+			case 1019: return FRIDAY_HARBOR_RSN + SEP + SIDNEY_BC_RSN; // Friday Harbor - Sidney B.C.
+			case 1117: return COUPEVILLE_RSN + SEP + PORT_TOWNSEND_RSN; // Coupeville - Port Townsend
+			case 1310: return LOPEZ_ISLAND_RSN + SEP + FRIDAY_HARBOR_RSN; // Lopez Island - Friday Harbor
+			case 1315: return LOPEZ_ISLAND_RSN + SEP + ORCAS_ISLAND_RSN; // Lopez Island - Orcas Island
+			case 1318: return LOPEZ_ISLAND_RSN + SEP + SHAW_ISLAND_RSN; // Lopez Island - Shaw Island
+			case 1510: return ORCAS_ISLAND_RSN + SEP + FRIDAY_HARBOR_RSN; // Orcas Island - Friday Harbor
+			case 1513: return ORCAS_ISLAND_RSN + SEP + LOPEZ_ISLAND_RSN; // Orcas Island - Lopez Island
+			case 1518: return ORCAS_ISLAND_RSN + SEP + SHAW_ISLAND_RSN; // Orcas Island - Shaw Island
+			case 1621: return POINT_DEFIANCE_RSN + SEP + TAHLEQUAH_RSN; // Point Defiance - Tahlequah
+			case 1711: return PORT_TOWNSEND_RSN + SEP + COUPEVILLE_RSN; // Port Townsend - Coupeville
+			case 1810: return SHAW_ISLAND_RSN + SEP + FRIDAY_HARBOR_RSN; // Shaw Island - Friday Harbor
+			case 1813: return SHAW_ISLAND_RSN + SEP + LOPEZ_ISLAND_RSN; // Shaw Island - Lopez Island
+			case 1815: return SHAW_ISLAND_RSN + SEP + ORCAS_ISLAND_RSN; // Shaw Island - Orcas Island
+			case 1910: return SIDNEY_BC_RSN + SEP + FRIDAY_HARBOR_RSN; // Sidney B.C. - Friday Harbor'
+			case 2022: return SOUTHWORTH_RSN + SEP + VASHON_ISLAND_RSN; // Southworth - Vashon Island
+			case 2116: return TAHLEQUAH_RSN + SEP + POINT_DEFIANCE_RSN; // Tahlequah - Point Defiance
+			case 2220: return VASHON_ISLAND_RSN + SEP + SOUTHWORTH_RSN; // Vashon Island - Southworth
 			// @formatter:on
 			}
 			System.out.printf("\nUnexpected route short name %s!\n", gRoute);
@@ -211,6 +216,10 @@ public class WashingtonStateFerryAgencyTools extends DefaultAgencyTools {
 
 	private static final Pattern TO = Pattern.compile("((^|\\W){1}(to)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
 
+	private static final String BC_SHORT = "BC";
+	private static final Pattern BC = Pattern.compile("((^|\\W){1}(b\\.c\\.)(\\W|$){1})", Pattern.CASE_INSENSITIVE);
+	private static final String BC_REPLACEMENT = "$2" + BC_SHORT + "$4";
+
 	@Override
 	public String cleanTripHeadsign(String tripHeadsign) {
 		Matcher matcherTO = TO.matcher(tripHeadsign);
@@ -219,6 +228,7 @@ public class WashingtonStateFerryAgencyTools extends DefaultAgencyTools {
 			tripHeadsign = gTripHeadsignAfterTO;
 		}
 		tripHeadsign = CleanUtils.cleanSlashes(tripHeadsign);
+		tripHeadsign = BC.matcher(tripHeadsign).replaceAll(BC_REPLACEMENT);
 		tripHeadsign = CleanUtils.CLEAN_AT.matcher(tripHeadsign).replaceAll(CleanUtils.CLEAN_AT_REPLACEMENT);
 		tripHeadsign = CleanUtils.CLEAN_AND.matcher(tripHeadsign).replaceAll(CleanUtils.CLEAN_AND_REPLACEMENT);
 		tripHeadsign = CleanUtils.cleanNumbers(tripHeadsign);
