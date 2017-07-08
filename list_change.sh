89e04447756c987c13307299e723f8847180c16e
Mathieu Méa <mathieu.mea@gmail.com>
2017-07-08 16:16:54 -0400

    Improve list change > grep returns 1 when no result

--- list_change.sh
+++ list_change.sh
@@ -5,7 +5,9 @@ TARGET=$(cat "change_directory");
 RESULT=$(git -C $TARGET status);
 checkResult $? false;
 RESULT=$(echo $RESULT | grep "res/raw");
-checkResult $? false;
+STATUS=$?;
+if [ $STATUS == 1 ]; then STATUS=0; fi; # grep returns 1 when no result
+checkResult $STATUS false;
 git -C $TARGET diff res/values/gtfs_rts_values_gen.xml
 checkResult $?;
 RESULT=$(git -C $TARGET diff-index  --name-only HEAD -- "res/raw" | wc -l);
