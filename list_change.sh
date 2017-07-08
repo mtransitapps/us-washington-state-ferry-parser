03f6f71779e766f1fe2dc02b3a4ba8146a8e7c85
Mathieu Méa <mathieu.mea@gmail.com>
2017-07-08 15:59:09 -0400

    Improve list change script.

--- list_change.sh
+++ list_change.sh
@@ -4,8 +4,14 @@ echo ">> Listing change...";
 TARGET=$(cat "change_directory");
 RESULT=$(git -C $TARGET status);
 checkResult $? false;
-RESULT=$(cat $RESULT | grep "res/raw");
+RESULT=$(echo $RESULT | grep "res/raw");
 checkResult $? false;
 git -C $TARGET diff res/values/gtfs_rts_values_gen.xml
 checkResult $?;
+RESULT=$(git -C $TARGET diff-index  --name-only HEAD -- "res/raw" | wc -l);
+if [ "$RESULT" -gt 0 ]; then
+	echo "> SCHEDULE CHANGED > MANUAL FIX!";
+	git -C $TARGET status | grep "res/raw" | head -n 7;
+	exit -1;
+fi
 echo ">> Listing change... DONE";
