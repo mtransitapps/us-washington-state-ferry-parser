29bdcb28b3e9b1b108cdcf3ea24066b283aed8a8
Mathieu Méa <mathieu.mea@gmail.com>
2018-07-14 01:13:04 -0400

    Compatible with Current/Next GTFS data

--- parse.sh
+++ parse.sh
@@ -2,9 +2,8 @@
 source ../commons/commons.sh
 echo ">> Parsing...";
 PARSER_DIRECTORY="../parser";
-PARSER_CLASSPATH=$(cat "$PARSER_DIRECTORY/classpath")
-CLASS=$(cat "parser_class");
 PARSER_CLASSPATH=$(cat "$PARSER_DIRECTORY/classpath");
+CLASS=$(cat "parser_class");
 java -Xms2048m -Xmx8192m -Dfile.encoding=UTF-8 \
 -classpath \
 bin:\
