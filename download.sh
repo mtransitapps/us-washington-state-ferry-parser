728dfdb6bf7f8e43090adc895297073e5f8a1904
Mathieu Méa <mathieu.mea@gmail.com>
2018-08-23 11:25:48 -0400

    New universal download.sh script compatible with different current/next URLs.

--- download.sh
+++ download.sh
@@ -2,19 +2,11 @@
 source ../commons/commons.sh
 echo ">> Downloading..."
 URL=`cat input_url`;
-FILENAME=$(basename "$URL");
-if [ -e input/gtfs.zip ]; then
-    mv input/gtfs.zip $FILENAME;
-    checkResult $?;
-    wget --header="User-Agent: MonTransit" --timeout=60 --tries=6 -N $URL;
-    checkResult $?;
-else
-    wget --header="User-Agent: MonTransit" --timeout=60 --tries=6 -S $URL;
-    checkResult $?;
-fi;
-mkdir -p input;
-if [ -e $FILENAME ]; then
-    mv $FILENAME input/gtfs.zip;
-    checkResult $?;
-fi;
+download $URL "input/gtfs.zip";
+checkResult $?;
+if [ -e "input_url_next" ]; then
+	URL=`cat input_url_next`;
+	download $URL "input/gtfs_next.zip";
+	checkResult $?;
+fi
 echo ">> Downloading... DONE"
